40367d2fb8f3041e3d27689d897f7a7c6eba4192
artjune Jeong <artjune@gmail.com>
2026-05-31 08:08:03 +0000

    local storage 저장

--- pages/Page1/jsobjects/app/app.js
+++ pages/Page1/jsobjects/app/app.js
@@ -1,15 +1,17 @@
 export default {
 	init () {
-		data.todos = data.dummyTodos;
+		data.todos = appsmith.store.todos || data.dummyTodos;
 	},
 	updateTodo (id, update) {
 		data.todos [id] = {...data.todos [id], ...update};
+		storeValue('todos', data.todos);
 	},
 	addTodo (title) {
 		this.updateTodo (data. todos. length, {title, completed: false}) ;
 	},
 	deleteTodo (id) {
 		data.todos.splice(id, 1);
+		storeValue('todos', data.todos);
 	},
 	setActiveTodo(id) {
 		console.log("setActiveTodo");
